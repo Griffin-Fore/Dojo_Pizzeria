bde0da8a7acf79ca995eaee8a5ac72ec70e94d7c
Griffin Fore <griffinfore@gmail.com>
2023-05-20 17:55:05 -0700

    Pizza Time!

--- Dojo_Pizzeria.js
+++ Dojo_Pizzeria.js
@@ -1,3 +1,4 @@
+
 function pizzaOven(crustType, sauceType, cheeses, toppings){
     var pizza = {};
     pizza.crustType = crustType;
@@ -24,7 +25,7 @@ meatloversPizza = {
     "crustType" : "cheese-in-the",
     "sauceType" : "traditional",
     "cheeses" : ["mozzarella","gorgonzola","parmesan","feta"],
-    "toppings" :["pepeeroni","sausage","bacon","canadian_bacon","black_olives"]
+    "toppings" :["pepperoni","sausage","bacon","canadian_bacon","black_olives"]
 };
 spinachPizza = {
     "crustType" : "hand-tossed",
@@ -34,3 +35,37 @@ spinachPizza = {
 };
 
 // Bonus Challenge: Create a function called randomPizza that uses Math.random() to create a random pizza!
+
+// create arrays that hold options
+var pizzaCrusts = ["traditional","deep-dish","hand-tossed"];
+var pizzaSauces = ["traditional","marinara","alfredo"];
+var pizzaCheeses = ["mozzarella","gorgonzola","feta","parmesan"];
+var pizzaToppings = ["pepperoni","sausage","bacon","canadian-bacon","black-olives","spinach"];
+// Math.round(Math.random() * toppings.length)
+
+function randomInt(num){
+    // floor sets the random values to whole numbers
+    var randomInt = Math.floor(Math.random() * num);
+
+    return randomInt;
+}
+
+randomInt()
+
+var pizzaCrust = pizzaCrusts[randomInt(pizzaCrusts.length-1)];
+var pizzaSauce = pizzaSauces[randomInt(pizzaSauces.length-1)];
+var pizzaCheese = pizzaCheeses[randomInt(pizzaCheeses.length-1)];
+var pizzaTopping = pizzaToppings[randomInt(pizzaToppings.length-1)];
+
+
+function randomPizza(){
+    var randomPizza = {
+        "crustType" : pizzaCrust,
+        "sauceType" : pizzaSauce,
+        "cheeseType" : pizzaCheese,
+        "pizzaTopping" : pizzaTopping
+    }
+    return randomPizza;
+}
+
+console.log(randomPizza());
